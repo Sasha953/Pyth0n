12151ac7d0025501c75de6462690c94e5fe1ac34
Sasha <sasha.ponomarenko.ua@gmail.com>
2021-02-15 16:25:01 +0200

    First commit

---
+++ For and while loops.py
@@ -0,0 +1,7 @@
+i = 5
+while i < 15:
+    print(i)
+    i = i + 2
+
+for i in 'hello world':
+    print(i * 2, end='')
--- Синтаксис.py
+++ Синтаксис.py
@@ -3,4 +3,4 @@ b = 2
 c = 3
 d = 4
 
-if (a == 1 and b == 2 and c == 3 and d == 4): print('Правильно!')
+if (a == 1 and b == 2 and c == 3 and d == 4): print('Правильно!', )
