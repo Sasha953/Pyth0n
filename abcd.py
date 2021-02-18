003b461fd15ec2740f67f08f0d147cfc97870a72
Sasha <sasha.ponomarenko.ua@gmail.com>
2021-02-18 18:48:38 +0200

    First commit

---
+++ abcd.py
@@ -0,0 +1,13 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+i = 0
+
+while i < len(a):
+    if a[i] < 5:
+        print(a[i])
+    i = i +1
+
+print()
+
+for elem in a:
+    if elem < 5:
+        print(elem)
