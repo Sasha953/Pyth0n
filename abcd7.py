d3713ce57ec17ba8845327e1abeda9665361f286
Sasha <sasha.ponomarenko.ua@gmail.com>
2021-02-25 18:53:42 +0200

    commit

---
+++ abcd7.py
@@ -0,0 +1,15 @@
+list = input().split()
+
+j = 0
+
+for j in range(len(list)):
+
+    for i in range(len(list) -1):
+
+        if list[i] > list[i + 1]:
+
+            list[i], list[i + 1] = list[i + 1], list[i]
+
+        i += 1
+
+print (list)
