139c6ec96e5efc6f33e27893ec8ebbb430ba8e69
Sasha <sasha.ponomarenko.ua@gmail.com>
2021-02-25 19:30:17 +0200

    commit

--- abcd7.py
+++ abcd7.py
@@ -3,13 +3,8 @@ list = input().split()
 j = 0
 
 for j in range(len(list)):
-
     for i in range(len(list) -1):
-
         if list[i] > list[i + 1]:
-
             list[i], list[i + 1] = list[i + 1], list[i]
-
         i += 1
-
 print (list)
